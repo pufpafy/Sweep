18016c9023a7e824d9d104182b35120495dffd37
pufpafy <fdeliter@abv.bg>
2017-02-23 13:41:12 +0200

    arduino project added

---
+++ Sweep.ino
@@ -0,0 +1,48 @@
+/* Sweep
+ by BARRAGAN <http://barraganstudio.com>
+ This example code is in the public domain.
+
+ modified 8 Nov 2013
+ by Scott Fitzgerald
+ http://www.arduino.cc/en/Tutorial/Sweep
+*/
+  
+  
+#include <Servo.h>
+
+
+Servo myservo;  // create servo object to control a servo
+// twelve servo objects can be created on most boards
+
+int pos = 0;    // variable to store the servo position
+
+void setup() {
+
+pinMode(13, OUTPUT);      // sets the digital pin as output
+  pinMode(12, OUTPUT);      // sets the digital pin as output
+  pinMode(11, OUTPUT);      // sets the digital pin as output
+  pinMode(10, OUTPUT);   // sets the digital pin as output
+   
+  myservo.attach(9);  // attaches the servo on pin 9 to the servo object
+}
+
+void loop() {
+
+digitalWrite(13, LOW);  //mux a 
+
+ digitalWrite(12, LOW);  // mux b 
+ digitalWrite(11, LOW);  //mux c
+ digitalWrite(10, LOW);  // mux d
+ 
+  
+  for (pos = 0; pos <= 180; pos += 1) { // goes from 0 degrees to 180 degrees
+    // in steps of 1 degree
+    myservo.write(pos);              // tell servo to go to position in variable 'pos'
+    delay(15);                       // waits 15ms for the servo to reach the position
+  }
+  for (pos = 180; pos >= 0; pos -= 1) { // goes from 180 degrees to 0 degrees
+    myservo.write(pos);              // tell servo to go to position in variable 'pos'
+    delay(15);                       // waits 15ms for the servo to reach the position
+  }
+}
+
